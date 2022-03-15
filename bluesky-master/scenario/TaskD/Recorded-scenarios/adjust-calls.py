f3e656f549eb59143cfd8d851f0c3354ea44e204
jilles <jilles@ziggo.nl>
2022-03-15 09:40:33 +0100

    small filtered improvements

--- bluesky-master/scenario/TaskD/Recorded-scenarios/adjust-calls.py
+++ bluesky-master/scenario/TaskD/Recorded-scenarios/adjust-calls.py
@@ -9,13 +9,13 @@ note = 0
 for count, line in enumerate(file):
     if 'ADDWPT IAFNW' in line:
         newfile.write(line[:19] + line[16:-35] + "call taskD/iafnw \r")
-        note = count + 9
+        note = count + 4
     if 'ADDWPT IAFNE' in line:
         newfile.write(line[:19] + line[16:-35] + "call taskD/iafne \r")
-        note = count + 9
+        note = count + 7
     if 'ADDWPT IAFSW' in line:
         newfile.write(line[:19] + line[16:-35] + "call taskD/iafsw \r")
-        note = count + 9
+        note = count + 5
     if 'ADDWPT IAFSE' in line:
         newfile.write(line[:19] + line[16:-35] + "call taskD/iafse \r")
         note = count + 9
