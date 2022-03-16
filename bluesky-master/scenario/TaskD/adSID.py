f2e9a8846145b763200aed1d452537ea362aed74
jilles <jilles@ziggo.nl>
2022-03-16 11:47:48 +0100

    gewerkt aan EHAM, paar foutjes in SID calling opgelost, nog moeite met separation

--- bluesky-master/scenario/TaskD/adSID.py
+++ bluesky-master/scenario/TaskD/adSID.py
@@ -6,13 +6,15 @@ for line in file:
     # print(line[-10:])
     # break
     newFile.write(line)
-    if '52.3045649 4.77753518' in line:
+    if '52.316237886827594 4.779730245101439' in line:
+        # Than we have toe define colour
+        newFile.write(line[:12] + line[16:-49] + "call taskD/SIDkaag \r")
+
+    elif '52.3045649 4.77753518' in line:
         # Than we have toe define colour
         newFile.write(line[:12] + line[16:-35] + "call taskD/SIDkaag \r")
 
-    if '52.316237886827594 4.779730245101439' in line:
-        # Than we have toe define colour
-        newFile.write(line[:12] + line[16:-35] + "call taskD/SIDkaag \r")
+
 
 file.close()
 newFile.close()
